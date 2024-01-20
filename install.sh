fe6669a70e38839e5dabc55d209c91b9aa27d3a0
arch-fan <juanserracines@gmail.com>
2024-01-20 12:11:27 +0100

    Remove comments

--- install.sh
+++ install.sh
@@ -6,13 +6,10 @@ then
     exit 1
 fi
 
-# Tomar la versión de neovim como el primer argumento del script
 NEOVIM_VERSION=${1:-latest}
 
-# Construir la imagen con el argumento proporcionado o 'latest' si no se proporciona ninguno
 docker build --build-arg NEOVIM_VERSION=${NEOVIM_VERSION} -t neovim-build .
 
-# El resto del script sigue igual
 docker run -d --name neovim-build neovim-build
 docker cp neovim-build:/usr/local/bin/nvim /usr/local/bin/nvim
 docker cp neovim-build:/usr/local/share/nvim/runtime/ /usr/local/share/nvim/runtime/
