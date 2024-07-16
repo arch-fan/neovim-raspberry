7209edd79a2d00324afeaa994a3b9d7f25f86e5e
arch-fan <juanserracines@gmail.com>
2024-07-16 10:29:22 +0200

    Modify🔧: Fetch Dockerfile from repo

--- remote-install.sh
+++ remote-install.sh
@@ -8,28 +8,12 @@ fi
 
 NEOVIM_VERSION=${1:-latest}
 
-DOCKERFILE_CONTENT=$(cat <<EOF
-FROM arm64v8/debian:latest
+DOCKERFILE_CONTENT=$(curl -s "https://raw.githubusercontent.com/arch-fan/neovim-raspberry/main/Dockerfile")
 
-ARG NEOVIM_VERSION=latest
-
-RUN apt-get update && apt-get install -y \
-    git cmake ninja-build libtool libtool-bin autoconf automake pkg-config unzip gettext \
-    && rm -rf /var/lib/apt/lists/*
-
-RUN if [ "$NEOVIM_VERSION" = "latest" ]; then \
-    git clone --depth 1 https://github.com/neovim/neovim; \
-    else \
-    git clone --branch ${NEOVIM_VERSION} --depth 1 https://github.com/neovim/neovim; \
-    fi
-
-WORKDIR /neovim
-RUN make CMAKE_BUILD_TYPE=RelWithDebInfo
-RUN cd build && cpack -G DEB
-
-CMD ["echo", "nothing to look here"]
-EOF
-)
+if [ -z "$DOCKERFILE_CONTENT" ]; then
+    echo "Failed to download Dockerfile content."
+    exit 1
+fi
 
 echo "$DOCKERFILE_CONTENT" | docker build --build-arg NEOVIM_VERSION=${NEOVIM_VERSION} -t neovim-build -
 
